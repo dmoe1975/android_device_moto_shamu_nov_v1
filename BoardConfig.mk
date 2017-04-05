d7ce01efede976bde934fc828a2d5ad795b81255
Michael Schwartz <schwartzmi@google.com>
2017-04-05 13:40:08 -0700

    Declare location of device manifest
    Bug: 36810913
    Test: make and load on device.
    
    Change-Id: I3bb81a8c1fc74b676e97616114eb7f6f5eea1fce

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,6 +137,8 @@ BOARD_USES_QC_TIME_SERVICES := true
 
 TARGET_FS_CONFIG_GEN += device/moto/shamu/config.fs
 
+DEVICE_MANIFEST_FILE := device/moto/shamu/manifest.xml
+
 -include vendor/moto/shamu/BoardConfigVendor.mk
 
 # Recovery
