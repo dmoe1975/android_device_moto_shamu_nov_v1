LineageOS/android_device_huawei_angler

Declare location of device manifest
Bug: 36810913
Test: make and load on device.

Change-Id: I3bb81a8c1fc74b676e97616114eb7f6f5eea1fce

BoardConfig.mk
@@ -141,4 +141,6 @@ USE_CLANG_PLATFORM_BUILD := true
 
 TARGET_FS_CONFIG_GEN += device/huawei/angler/config.fs
 
+DEVICE_MANIFEST_FILE := device/huawei/angler/manifest.xml
+
 -include vendor/huawei/angler/BoardConfigVendor.mk

dmoe1975/android_device_moto_shamu_nov_v1

BoardConfig.mk
@@ -137,6 +137,8 @@ BOARD_USES_QC_TIME_SERVICES := true
 
 TARGET_FS_CONFIG_GEN += device/moto/shamu/config.fs
 
+DEVICE_MANIFEST_FILE := device/moto/shamu/manifest.xml
+
 -include vendor/moto/shamu/BoardConfigVendor.mk
 
 # Recovery